70b3374f366f5b8b8fb59ea5e4750edd9eb64820
Robert <rlinden@g.hmc.edu>
2019-03-02 18:46:48 -0800

    Can't get react to work for me; need to gain a better understanding

--- static/load.js
+++ static/load.js
@@ -1,9 +1,29 @@
-td = document.createElement('td')
-// document.getElementById('graph').appendChild()
+sheet = document.getElementById('sheet');
+console.log(sheet);
+tr = document.createElement('tr');
+td = document.createElement('td');
+td.innerText = 'test';
 
-const domContainer = document.querySelector('#sheet');
-ReactDOM.render(e(P), domContainer);
+tr.append(td);
+sheet.appendChild(tr)
 
-function testclick() {
-    console.log('try this?');
-}
+// class Tile extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.index = props.index
+//     }
+
+//     render() {
+//         return(
+//             <td>{this.index}</td>
+//         );
+//     }
+// }
+
+
+// const e = React.createElement;
+// const sheet = document.querySelector('#sheet');
+// tr = e('tr', {id: 'tr-0'});
+// ReactDom.render(tr, sheet);
+// td = e(Tile, {index: (0)});
+// ReactDom.render(td, tr);
