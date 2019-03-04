7724008179e27b329324733e1379b4a0fe11ffc0
Robert <rlinden@g.hmc.edu>
2019-03-03 16:14:20 -0800

    JSX not JVX

--- static/load.js
+++ static/load.js
@@ -1,29 +1,17 @@
-sheet = document.getElementById('sheet');
-console.log(sheet);
-tr = document.createElement('tr');
-td = document.createElement('td');
-td.innerText = 'test';
+// sheet = document.getElementById('sheet');
+// console.log(sheet);
+// tr = document.createElement('tr');
+// td = document.createElement('td');
+// td.innerText = 'test';
 
-tr.append(td);
-sheet.appendChild(tr)
+// tr.append(td);
+// sheet.appendChild(tr)
 
-// class Tile extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.index = props.index
-//     }
-
-//     render() {
-//         return(
-//             <td>{this.index}</td>
-//         );
-//     }
-// }
+function Text(props) {
+    return <p>{props.text}</p>
+}
 
 
-// const e = React.createElement;
-// const sheet = document.querySelector('#sheet');
-// tr = e('tr', {id: 'tr-0'});
-// ReactDom.render(tr, sheet);
-// td = e(Tile, {index: (0)});
-// ReactDom.render(td, tr);
+const e = React.createElement;
+const dom = document.querySelector('body');
+ReactDOM.render(e(Text, {text: 'text'}), dom);
