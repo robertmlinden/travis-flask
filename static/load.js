ccd513a2caac17023643cd8aa1d27376c710c334
Robert <rlinden@g.hmc.edu>
2019-03-05 19:12:25 -0800

    Small change

--- static/load.js
+++ static/load.js
@@ -4,31 +4,30 @@ function Text(props) {
 
 const e = React.createElement;
 
-function Table(props) {
-    return  <tbody>{props.rows}</tbody>
+function Spreadsheet(props) {
+    var rows = [];
+    for(var i = 0; i < 10; i++) {
+        var cols = [];
+        for(var j = 0; j < 10; j++) {
+            var cell = <Cell index={i*10 + j} />;
+            cols.push(cell);
+        }
+        var row = <Row row={i} cells={cols} />
+        rows.push(row);
+    }
+    return  <table><tbody>{rows}</tbody></table>
 }
 
 function Row(props) {
-    return <tr id={props.row}>{props.cells}</tr>;
+    return <tr key={props.row} id={"row-" + props.row}>{props.cells}</tr>;
 }
 
 function Cell(props) {
-    return <td>{props.i}</td>;
+    return <td key={props.index} id={"cell-" + props.index}>{props.index}</td>;
 }
 
-var rows = [];
-for(var i = 0; i < 10; i++) {
-    var cols = [];
-    for(var j = 0; j < 10; j++) {
-        var cell = e(Cell, {i: i*10 + j});
-        cols.push(cell);
-    }
-    var row = <Row row={"row" + i} cells={cols} />
-    rows.push(row);
-}
-var table = e(Table, {rows: rows});
+var table = e(Spreadsheet, {});
 ReactDOM.render(table, document.getElementsByTagName('body')[0]);
-console.log(rows);
 
 
 // const dom = document.querySelector('body');
